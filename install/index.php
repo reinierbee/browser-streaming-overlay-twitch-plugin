6c27c344b22b6fad3b4680bab0852ba49cfd96a5
Reinier B <ssjkrillen@hotmail.com>
2014-09-23 19:29:34 +0200

    Composer installation

--- install/index.php
+++ install/index.php
@@ -6,21 +6,9 @@
  * Time: 2:43 PM
  */
 
-exec('pear install File_Archive',$output);
+print_r("Starting installation... \n\n");
+
+passthru("php ../composer.phar install",$output);
+passthru("php ../composer.phar update",$output);
 
 print_r($output);
-
-//require_once "File/Archive.php";
-
-exec('dir',$output);
-print_r($output);
-
-$zip = new ZipArchive();
-
-if ($zip->open('easy-twitch-javascript-api-master.zip') === TRUE) {
-    $zip->extractTo('/temp/');
-    $zip->close();
-    echo 'unzipping done...';
-} else {
-    echo 'Could not open zip file or was not present';
-}
