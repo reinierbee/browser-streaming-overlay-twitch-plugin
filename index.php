111455da6892ebf3db6393a0bef2259ebcc6afdc
Reinier Boon <rboon@bol.com>
2014-10-29 17:29:41 +0100

    nothing
    Change-Id: I49ece2eeb2157934d6ac0d4849d376f3fabb178d

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ require("bootstrap.php");
 
 $app = new \Slim\Slim();
 $app->config('debug', true);
-$_SERVER['REQUEST_METHOD'] = "GET";
+/*$_SERVER['REQUEST_METHOD'] = "GET";
 $_SERVER['REMOTE_ADDR'] = "127.0.0.1";
 $_SERVER['REQUEST_URI'] = "/browser-streaming-overlay-twitch-plugin/index.php/api/asda";
 $_SERVER['SERVER_NAME'] = "PhpStorm 8.0.1";
 $_SERVER['SERVER_PORT'] = "80";
 $_SERVER['PATH_INFO'] = "/api/asda";
-
+*/
 
 $app->get('/api/:name', function ($name) {
     echo "Hello, $name";
